44ed396a6b090bdcad1ac2f9e8ed7bef80b8da85
storm <stooormix@gmail.com>
2022-06-22 00:07:53 +0430

    create work space report

--- src/component/Dashboard/DashboaedComponents/workSpace/workSpaceReport.jsx
+++ src/component/Dashboard/DashboaedComponents/workSpace/workSpaceReport.jsx
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react'
 
-export default function WorkSpaceReport({websiteName}) {
+export default function WorkSpaceReport({ websiteName }) {
+    // const [{site:"https://example.ir/page1",key:"a"}]
+    const data = [{ site: "https://example.ir/page1", key: "a" }, { site: "https://example.ir/page1", key: "a" }]
+    // const ahmad={fullName:"ddd",age:17};
     return (
         <Fragment>
 
@@ -16,15 +19,75 @@ export default function WorkSpaceReport({websiteName}) {
                         <p className=' text-xl font-bold text-[#fff] mb-3'>موفقیت آمیز!</p>
                         <p className=' text-xl font-bold text-[#fff]'>کاربر عزیز ورک‌اسپیس جدید شما با موفقیت ایجاد شد!</p>
                     </div>
-                    <img src="/img/dashboard/workSpace/headMessageReport/workSpace_report_ico.svg" alt="" className=''/>
+                    <img src="/img/dashboard/workSpace/headMessageReport/workSpace_report_ico.svg" alt="" className='' />
                 </div>
-                <div className=' w-full p-48'>
-                    <div className=' pl-5 py-2'>
-                        <div className='flex gap-6 items-center relative'>
+                {/* web site name */}
+                <div className='w-full px-48'>
+                    <div className=' pl-5 py-2 border border-[#D9D9D9] mt-7'>
+                        <div className='flex gap-6 justify-between items-center relative'>
                             <div className='w-[20px] h-[2px] bg-[#0A65CD] rotate-90 rounded absolute -right-[10px]' />
                             <span className=' text-sm mr-3'>آدرس وبسایت شما: </span>
+                            <span className='text-sm float-left'>https://example.ir</span>
                         </div>
-                        <span className='text-sm'>{websiteName}</span>
+                    </div>
+                    {/* site key words */}
+                    <div className=' pl-5 py-2 border border-[#D9D9D9] mt-7'>
+                        <div className='flex gap-6 justify-between items-center relative'>
+                            <div className='w-[20px] h-[2px] bg-[#0A65CD] rotate-90 rounded absolute -right-[10px]' />
+                            <span className=' text-sm mr-3'>کلمات کلیدی وبسایت:</span>
+                        </div>
+                        <div className=' flex justify-between px-5 mt-2'>
+                            <div className=' w-2/4 pl-3'>
+                                <div className='pl-2'>
+                                    <p className=' w-full text-right text-xs'>سایت مرتبط</p>
+                                    <p className=' w-full text-left text-sm mt-3'>https://example.ir/page1</p>
+                                </div>
+                            </div>
+                            <div className=' w-2/4 pr-3'>
+                                <div className='pr-2'>
+                                    <p className=' w-full text-right text-xs'>سایت مرتبط</p>
+                                    <p className=' w-full text-left text-sm mt-3'>https://example.ir/page1</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* site commercial page */}
+                    <div className=' pl-5 py-2 border border-[#D9D9D9] mt-7'>
+                        <div className='flex gap-6 justify-between items-center relative'>
+                            <div className='w-[20px] h-[2px] bg-[#0A65CD] rotate-90 rounded absolute -right-[10px]' />
+                            <span className=' text-sm mr-3'>صفحات تجاری وبسایت:</span>
+                        </div>
+                        <div className='px-5 mt-2 w-full'>
+                            <div className=''>
+                                <p className=' w-full text-right text-xs'>صفحه تجاری با اولویت یک </p>
+                                <p className=' w-full text-left text-sm mt-3'>https://example.ir/page1</p>
+                            </div>
+                        </div>
+                        <div className='px-5 mt-2 w-full'>
+                            <div className=''>
+                                <p className=' w-full text-right text-xs'>صفحه تجاری با اولویت دو</p>
+                                <p className=' w-full text-left text-sm mt-3'>https://example.ir/page1</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* page test speed */}
+                    <div className=' pl-5 py-2 border border-[#D9D9D9] mt-7'>
+                        <div className='flex gap-6 justify-between items-center relative'>
+                            <div className='w-[20px] h-[2px] bg-[#0A65CD] rotate-90 rounded absolute -right-[10px]' />
+                            <span className=' text-sm mr-3'>مانیتورینگ سرعت صفحات</span>
+                        </div>
+                        <div className='px-5 mt-2 w-full'>
+                            <div className=''>
+                                <p className=' w-full text-right text-xs'>آدرس صفحه</p>
+                                <p className=' w-full text-left text-sm mt-3'>https://example.ir/page1</p>
+                            </div>
+                        </div>
+                        <div className='px-5 mt-2 w-full'>
+                            <div className=''>
+                                <p className=' w-full text-right text-xs'>آدرس صفحه</p>
+                                <p className=' w-full text-left text-sm mt-3'>https://example.ir/page1</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
