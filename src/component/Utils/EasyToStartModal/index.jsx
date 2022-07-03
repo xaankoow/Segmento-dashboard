58ccefef4775f9e163cec8005fab0c577a1e917e
parsamanesh <stooormix@gmail.com>
2022-07-03 14:42:59 +0430

    edite footerBtn &bodyContent and add check err buy plan after the buy plan handle step modal after the buy plan (!final)

--- src/component/Utils/EasyToStartModal/index.jsx
+++ src/component/Utils/EasyToStartModal/index.jsx
@@ -1,14 +1,29 @@
-import React, { useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import Modal from 'react-modal'
 import Head from './HeadModal'
 import BodyContent from './BodyContent'
 import FooterBtn from './FooterBtn'
+import AleartMessageBuyPlan from '../../Dashboard/DashboaedComponents/BuyPlan/AleartMessageBuyPlan'
 
-export default function BuyPlanEasyToStartModal({ handleClose, checkClose, show }) {
-  const [stepModal, setStepModal] = useState(6);
+export default function BuyPlanEasyToStartModal({ handleClose, checkClose, show, checkBuyPlan }) {
+  const [stepModal, setStepModal] = useState(1);
   // const [, setDiscount] = useState("sample-code");
   const [plan, setPlan] = useState("");
-  const [free, setFree] = useState(true);
+  const [free, setFree] = useState(false);
+  const [packageUuid, setPackageUuid] = useState("")
+  const [applyWebAdress, setApplyWebAdress] = useState("")
+  const [lockNextStep, setLockNextStep] = useState(false);
+  const [checkErr, setCheckErr] = useState(false);
+  // console.log(applyWebAdress)
+  // debugger
+  // useEffect(() => {
+  //   const find_buy_type = localStorage.getItem("buyType")
+  //   if (true & false) {
+  //     setStepModal(3);
+  //   } else if (true && true) {
+  //     setCheckErr(true)
+  //   }
+  // }, [])
 
   const customStyles = {
     content: {
@@ -35,11 +50,21 @@ export default function BuyPlanEasyToStartModal({ handleClose, checkClose, show 
       // className={"myModal"}
       >
         <div className='w-[907px]'>
-          <Head handleClose={handleClose} stepModal={stepModal} free={free} />
-          <div className={`${stepModal==1?"px-6":stepModal>2?"px-2":stepModal==2?" px-4":""}`}>
-            <BodyContent stepModal={stepModal} setPlan={setPlan} plan={plan} free={free} setFree={setFree} />
-            <FooterBtn stepModal={stepModal} setFree={setFree} setStepModal={setStepModal} handleClose={handleClose}/>
-          </div>
+          {checkErr ? (
+            <div className='h-[685px]'>
+              {
+              AleartMessageBuyPlan()
+              }
+            </div>
+          ) : (
+            <Fragment>
+              <Head handleClose={handleClose} stepModal={stepModal} free={free} />
+              <div className={`${stepModal == 1 ? "px-6" : stepModal > 2 ? "px-2" : stepModal == 2 ? " px-4" : ""}`}>
+                <BodyContent setStepModal={setStepModal} setApplyWebAdress={setApplyWebAdress} setPackageUuid={setPackageUuid} stepModal={stepModal} setPlan={setPlan} plan={plan} free={free} setFree={setFree} lockNextStep={lockNextStep} setLockNextStep={setLockNextStep} />
+                <FooterBtn applyWebAdress={applyWebAdress} stepModal={stepModal} free={free} setFree={setFree} setStepModal={setStepModal} handleClose={handleClose} lockNextStep={lockNextStep} packageUuid={packageUuid} />
+              </div>
+            </Fragment>
+          )}
         </div>
       </Modal>
     </div>
