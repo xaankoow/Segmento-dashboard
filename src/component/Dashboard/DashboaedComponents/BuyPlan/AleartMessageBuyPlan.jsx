3e5b3565b6d53594628ecab62c9fabee71cfc2c0
storm <stooormix@gmail.com>
2022-07-25 19:38:19 +0430

    fix show buy plan status (type,text,navigate)

--- src/component/Dashboard/DashboaedComponents/BuyPlan/AleartMessageBuyPlan.jsx
+++ src/component/Dashboard/DashboaedComponents/BuyPlan/AleartMessageBuyPlan.jsx
@@ -1,8 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getAllFinancialReports } from '../../../Redux/Action/financialReports';
 
 export default function AleartMessageBuyPlan() {
-    const typeAlert = "err";
+    
+    const { financialDataTable } = useSelector(state => state.financialState)
+    
+    const typeAlert =financialDataTable.length!=0?financialDataTable[0].payment_status_text=="پرداخت شده"?"success":"err":"";
+
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(getAllFinancialReports())
+    }, [])
 
     return (
         <Fragment>
@@ -10,11 +21,13 @@ export default function AleartMessageBuyPlan() {
                 <div className='w-[20px] h-[2px] bg-[#001F43] rotate-90 rounded absolute -right-[9px]' />
                 <span className='text-lg'>خرید اشتراک سگمنتو</span>
             </div>
+            {financialDataTable.length>0?(
             <div className=' px-10 mt-12'>
                 <div className={`flex justify-between pl-10 pr-4 py-5  ${typeAlert == "success" ? "bg-[#10CCAE]" : "bg-[#F35242]"} rounded-lg`}>
                     <div>
                         <p className=' text-xl font-bold text-[#fff] mb-3'>{typeAlert == "success" ? "موفقیت آمیز!" : "خطایی رخ داده!"}</p>
-                        <p className=' text-xl font-bold text-[#fff] mb-3'>{typeAlert == "success" ? "کاربر عزیز اشتراک 12 ماهه طلایی برای شما فعال شد :)" : "کاربر عزیز اشتراک شما فعال نشد :("}</p>
+                        {/* <p className=' text-xl font-bold text-[#fff] mb-3'>{typeAlert == "success" ? "کاربر عزیز اشتراک 12 ماهه طلایی برای شما فعال شد :)" : "کاربر عزیز اشتراک شما فعال نشد :("}</p> */}
+                        <p className=' text-xl font-bold text-[#fff] mb-3'>{financialDataTable[0].description}</p>
                         <p className=' text-lg font-normal text-[#fff]'>{typeAlert == "success" ? "از امکانات سگمنتو لذت ببرید." : "برای خرید یا فعالسازی مجدد اشتراک لطفا دوباره سعی کنید."}</p>
                     </div>
                     {typeAlert == "success" ? <img src="/img/dashboard/buyPlan/alert/success_ico.svg" alt="" className='' /> : <img src="/img/dashboard/buyPlan/alert/error_ico.svg" alt="" className='' />}
@@ -30,6 +43,7 @@ export default function AleartMessageBuyPlan() {
                     ) :<Link to={"/dashboard/buyPlan"} className='btn-style inline-block'>خرید مجدد اشتراک</Link>}
                 </div>
             </div>
+            ):null}
         </Fragment>
     )
 }
