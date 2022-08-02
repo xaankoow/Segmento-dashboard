b90f3683c98c3619c46fe7454deccecc59d69974
storm <stooormix@gmail.com>
2022-08-02 21:37:18 +0430

    force

--- src/component/Redux/Reducer/plan.js
+++ src/component/Redux/Reducer/plan.js
@@ -9,7 +9,7 @@ export const planReducer = (state =
         commercialPage2: "",
         planChosen: "",
         discount: "",
-        discountStatus: { value: 10, planType: "" },
+        discountStatus: { value: 0, planType: "" },
         packageUuid: "",
         forceUpdate: 0,
         planDetails: {},
