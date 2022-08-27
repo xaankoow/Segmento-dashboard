efcb0dd2e95c88f1dc1c8b5437328c2e0b6c9f2a
parsamanesh <stooormix@gmail.com>
2022-08-27 17:31:39 +0430

    add color to tailwind palet replace btn with Auth btn in buy plan section edite Auth button component

--- src/component/Dashboard/DashboaedComponents/EasyStart/EasyStart.jsx
+++ src/component/Dashboard/DashboaedComponents/EasyStart/EasyStart.jsx
@@ -8,21 +8,21 @@ const EasyStart = ({ startButtonClick }) => {
     return (
         <>
             <div className=" flex items-center justify-between my-9 mx-7 border  text-sm rounded-lg border-lightGray px-7 py-11">
-                <span>
+                <span className="text-primaryV1">
                 اگر سوالی دارید، با همکاران ما در واحد پشتیبانی تماس بگیرید.                </span>
                 <button className="btn-style"> برقراری تماس</button>
             </div>
             <div className="flex flex-col relative mt-9 mx-7 mb-7 ">
-                <div className="flex items-center justify-center bg-gold text-base py-5 absolute w-full -top-5  rounded-t-lg z-[2] ">
+                <div className="flex items-center justify-center text-title bg-gold text-base py-5 absolute w-full -top-5  rounded-t-lg z-[2] ">
                     شروع آسان 
                 </div>
                 <div className="rounded-lg border border-lightGray EasyStartBoxContent flex flex-col  px-7">
-                    <div className="flex flex-col gap-1 text-sm mx-7 mt-20 ">
-                        <span className="">
+                    <div className="flex flex-col gap-1 text-sm mx-7 mt-20">
+                        <span className="text-primaryV1">
                         چند گام کوچک از تمام قدرت سگمنتو فاصله دارید، 
                         </span>
-                        <span>    آدرس وب‌سایت و کلمات کلیدی خودتان را آماده کنید تا شروع کنیم.</span>
-                        <span>پس از انتخاب اشتراک و انجام فرایند شروع آسان به داده‌های تمیز و ساده دسترسی دارید که قابل استفاده و مطمئن هستند. </span>
+                        <span className="text-primaryV1">    آدرس وب‌سایت و کلمات کلیدی خودتان را آماده کنید تا شروع کنیم.</span>
+                        <span className="text-primaryV1">پس از انتخاب اشتراک و انجام فرایند شروع آسان به داده‌های تمیز و ساده دسترسی دارید که قابل استفاده و مطمئن هستند. </span>
                     </div>
                     <div className="flex justify-between items-center mt-16 ">
 
