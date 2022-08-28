419daac9ffdf40bc33174f5336ca9da266c0431a
storm <stooormix@gmail.com>
2022-08-29 00:33:22 +0430

    create modal default style in the variabes folder and using this

---
+++ src/variables/style.js
@@ -0,0 +1,14 @@
+
+export const defaultCustomModalStyle = {
+    
+    content: {
+      top: '43vh',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      backgrounColor: "red",
+      'z-index': '100'
+    },
+  };
