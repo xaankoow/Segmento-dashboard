b7969e7218f4cf9738e89635cb28799a939c6e61
storm <stooormix@gmail.com>
2022-09-01 01:54:43 +0430

    change paragraph text in buy plan section show pharagraph after the selected plan

--- src/component/Utils/buyPlanSection_UTILS/proposalPlanParagraph/HandleParagraphInfoPlan.jsx
+++ src/component/Utils/buyPlanSection_UTILS/proposalPlanParagraph/HandleParagraphInfoPlan.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useState } from 'react'
 import { Paragraph } from './createParagraph';
 
-export default function HandleParagraphInfoPlan({typePlan,indexPlan}) {
+export default function HandleParagraphInfoPlan({ typePlan, indexPlan }) {
     // const [paragraph,setParagraph]=useState("با خرید اشتراک 12 ماهه طلایی شما فقط مبلغ 10 ماه رو پرداخت میکنید؛ 2 ماه مهمون سگمنتو باشین");
-
+    const getParagraph = Paragraph(typePlan, indexPlan);
     return (
-        <div className='container_plan_message mt-5 rounded-lg'>
-            <img src="/img/modal/footer/planInfoMessage.svg" className='inline-block mr-3' alt="" />
-            <span className='py-2.5 mr-3 inline-block text-sm '>{Paragraph(typePlan,indexPlan)}</span>
-        </div>
+        <Fragment>
+            {getParagraph != "" ? (
+                <div className='container_plan_message mt-5 rounded-lg'>
+                    <img src="/img/modal/footer/planInfoMessage.svg" className='inline-block mr-3' alt="" />
+                    <span className='py-2.5 mr-3 inline-block text-sm '>{getParagraph}</span>
+                </div>
+            ) : null}
+        </Fragment>
     )
 }
