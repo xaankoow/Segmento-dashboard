5f6d5bb810b0aa9fe87fdac3d413b82d5ccc4d7b
parsamanesh <stooormix@gmail.com>
2022-09-07 18:32:19 +0430

    create close baner in support section

---
+++ src/component/Dashboard/DashboaedComponents/support/CloseSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CloseSection() {
+  return (
+    <div className='m-auto rounded-lg my-5 bg-red max-w-5xl'>
+        <p className=' py-7 text-xl text-center text-[#fff]'>این تیکت بسته شده است. جهت بازگشایی آن، باید پاسخی ارسال کنید.</p>
+    </div>
+  )
+}
