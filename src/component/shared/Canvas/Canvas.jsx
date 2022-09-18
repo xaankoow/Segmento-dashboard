5f6b6e226d2e3a07a941bf41ddcaa324fcc96d62
Fatemeh Sharififar <fatemehsharififar811@gmail.com>
2022-09-18 10:16:01 +0430

    some edit

--- src/component/shared/Canvas/Canvas.jsx
+++ src/component/shared/Canvas/Canvas.jsx
@@ -5,36 +5,36 @@ import { lineData } from "../../../variables/copyWritingFeature";
 export default function Canvas({}) {
   //   var c = document.getElementById("myCanvas");
   //   var ctx = c.getContext("2d");
-  const canvas = React.useRef();
+//   const canvas = React.useRef();
 
-  function creatediv(id, ax,ay, bx,by, size, opacity, color) { 
+//   function creatediv(id, ax,ay, bx,by, size, opacity, color) { 
 
-    var length = Math.hypot(by-ay, bx-ax),
-        deg    = Math.atan2(by-ay, bx-ax) * 180 / Math.PI;
+//     var length = Math.hypot(by-ay, bx-ax),
+//         deg    = Math.atan2(by-ay, bx-ax) * 180 / Math.PI;
 
-    var newdiv = document.createElement('div'); 
-    newdiv.setAttribute('id', id);  
-    newdiv.style.width =  length + "px";     
-    newdiv.style.height = (size||2) + "px";  
-    newdiv.style.left = ax + "px";   
-    newdiv.style.top = ay + "px";   
-    newdiv.style.background = color || "red";    
-    newdiv.style.opacity = opacity || 1;
-    newdiv.style.transformOrigin = "left 50%";
-    newdiv.style.transform = "rotate("+ deg +"deg)";
-    newdiv.style.position = "absolute";
+//     var newdiv = document.createElement('div'); 
+//     newdiv.setAttribute('id', id);  
+//     newdiv.style.width =  length + "px";     
+//     newdiv.style.height = (size||2) + "px";  
+//     newdiv.style.left = ax + "px";   
+//     newdiv.style.top = ay + "px";   
+//     newdiv.style.background = color || "red";    
+//     newdiv.style.opacity = opacity || 1;
+//     newdiv.style.transformOrigin = "left 50%";
+//     newdiv.style.transform = "rotate("+ deg +"deg)";
+//     newdiv.style.position = "absolute";
 
-    document.body.appendChild(newdiv); 
+//     document.body.appendChild(newdiv); 
 
-}
+// }
 
 
 
-  useEffect(() => {
-    creatediv("a", 20, 20, 200, 80);
-    creatediv("a", 20, 50, 200, 80);
+//   useEffect(() => {
+//     creatediv("a", 20, 20, 200, 80);
+//     creatediv("a", 20, 50, 200, 80);
     
-  });
+//   });
 
 
 
@@ -48,9 +48,10 @@ export default function Canvas({}) {
 
   return (
    
-      <div id="myCanvas"  width={"50%"} ref={canvas} className="border">
-        Your browser does not support the HTML5 canvas tag.
-      </div>
+      // <div id="myCanvas"  width={"50%"} ref={canvas} className="border">
+      //   Your browser does not support the HTML5 canvas tag.
+      // </div>
+      <div></div>
  
   );
 }
