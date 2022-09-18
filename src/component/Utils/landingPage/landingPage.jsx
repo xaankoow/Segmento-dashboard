63b9a74b6304486406478fe580735f0bdff2c41c
storm <stooormix@gmail.com>
2022-09-19 01:54:21 +0430

    handle get uuid in url browser

--- src/component/Utils/landingPage/landingPage.jsx
+++ src/component/Utils/landingPage/landingPage.jsx
@@ -4,17 +4,22 @@ import { checkDetailUuid } from '../../service/paymentService';
 
 export default function LandingPage() {
   useEffect(() => {
-    const pathBrowserOrg = window.location.search;
-    const urlParams = new URLSearchParams(pathBrowserOrg);
+    const pathBrowserOrg = window.location.href;
+    // const urlParams = new URLSearchParams(pathBrowserOrg);
+    const urlParams = new URLSearchParams("uuid=avc");
     const uuid = urlParams.get("uuid");
     checkUuid(uuid);
   }, [])
 
   const navigate = useNavigate();
 
-  const checkUuid = async (uuidResulte) => {
+  const checkUuid = async () => {
+
+    const getAndSplitHash=window.location.hash.split("?")   //?filter=a  a0ed15bb-3ba6-433a-ae2b-47249f60cf55
+    const uuidResulte=getAndSplitHash[1].substring(5,41)
+
     try {
-     
+      debugger
       const { data } = await checkDetailUuid(uuidResulte);
       if (data.data.payment_status_text == "پرداخت نشده") {
         localStorage.setItem("statusBuyPlna", false)
@@ -23,7 +28,7 @@ export default function LandingPage() {
       }
       navigate('/dashboard', { replace: true });
     } catch (error) {
-      
+
     }
   }
 
