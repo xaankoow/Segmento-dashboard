4851a3dba0844a53b9c2f4a839a43353c105990f
parsamanesh <stooormix@gmail.com>
2022-09-22 17:15:15 +0330

    install react-polygon using in page404(test)

--- src/component/Utils/Error404/page404.js
+++ src/component/Utils/Error404/page404.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router'
-
+import Polygon from 'react-polygon'
+import image1 from './img.svg'
 export default function Page404() {
 
   const navigate = useNavigate()
@@ -10,48 +11,82 @@ export default function Page404() {
 
     if (userToken) {
 
-      navigate("/dashboard", { replace: true })
-    }else{
-      navigate("/dashboard/accountOperations/login", { replace: true });
+      // navigate("/dashboard", { replace: true })
+    } else {
+      // navigate("/dashboard/accountOperations/login", { replace: true });
     }
   }, [])
 
+  
+
+  const  myRenderPoint =(point)=> {
+    return (
+      <>
+      <polygon points='30,400 370,200'  className='polygone_line_style1'/>
+      </>
+    )
+  }
+
+  // height: 492px;
+  //   width: 293px;
+  //   position: absolute;
+  //   right: 87px;
 
   return (
-    <div id='page-404'>
+    <div id='page-404' className=' bg-red-600'>
 
-{/* <svg width="3000" height="2000">
-<svg width="3000" height="2000">
-  <circle cx="30" cy="550" r="3"
-    fill="#D9D9D9" />
-      <circle cx="30" cy="520" r="3"
-    fill="#D9D9D9" />
-      <circle cx="30" cy="490" r="3"
-    fill="#D9D9D9" />
-      <circle cx="30" cy="460" r="3"
-    fill="#D9D9D9" />
-      <circle cx="30" cy="430" r="3"
-    fill="#D9D9D9" />
+
+<div className='polygone_line_style'>
+      {/* <img src={image1}/> */}
+      <svg width="400" height="650" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="30" cy="550" r="3"
+            fill="#D9D9D9" />
+          <circle cx="30" cy="520" r="3"
+            fill="#D9D9D9" />
+          <circle cx="30" cy="490" r="3"
+            fill="#D9D9D9" />
+          <circle cx="30" cy="460" r="3"
+            fill="#D9D9D9" />
+          <circle cx="30" cy="430" r="3"
+            fill="#D9D9D9" />
           <circle cx="30" cy="400" r="3"
-    fill="#D9D9D9" />
-</svg>
-      <polygon points="30,550 370,200"
-  style="stroke:#D9D9D9;stroke-width:1;" />
-    <polygon points="30,520 370,200"
-  style="stroke:#D9D9D9;stroke-width:1;" />
-      <polygon points="30,490 370,200"
-  style="stroke:#D9D9D9;stroke-width:1;" />
-      <polygon points="30,460 370,200"
-  style="stroke:#D9D9D9;stroke-width:1;" />
-      <polygon points="30,430 370,200"
-  style="stroke:#D9D9D9;stroke-width:1;" />
-      <polygon points="30,400 370,200"
-  style="stroke:#D9D9D9;stroke-width:1;" />
+            fill="#D9D9D9" />
+        <Polygon renderPoint={myRenderPoint}/>
+        </svg>
+         {/* <svg width="3000" height="2000" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="30,550 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,520 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,490 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,460 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,430 370,200"
+          style={"stroke:#D9D9D9;stroke-width:1;"} />
+        <polygon points="30,400 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+      </svg> */}
+      {/* <img src={image1} width="500" height={"500"}/> */}
+      </div>
+      {/* <svg width="3000" height="2000">
+        <polygon points="30,550 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,520 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,490 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,460 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,430 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+        <polygon points="30,400 370,200"
+          style="stroke:#D9D9D9;stroke-width:1;" />
+      </svg> */}
 
-</svg> */}
       {/* <p>داری دنبال زیر بغل مار میگردی؟</p>
         <img src='/images/404/snake_picture.jpg'/> */}
-      {/* <p>صفحه مورد نظر پیدا نشد!</p> */}
-    </div>
+  {/* <p>صفحه مورد نظر پیدا نشد!</p> */ }
+    </div >
   )
 }
