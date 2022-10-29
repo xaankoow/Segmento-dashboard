9210e40b389da0a758f87a8ef4e1269777605e46
parsamanesh <stooormix@gmail.com>
2022-10-29 13:02:06 +0330

    create ticket message api

--- src/component/service/ticket.js
+++ src/component/service/ticket.js
@@ -10,3 +10,12 @@ export const setNewTicket = datas => {
     }
     return http.post(`${config.xaankooApi}/api/v1/ticket`,datas, headerRegisterUser);
 }
+
+export const getSupportChatData = chatUuid => {
+    //7
+    const headerRegisterUser = {
+        'Content-Type': 'multipart/form-data',
+        "workspace":"text/plain"
+    }
+    return http.get(`${config.xaankooApi}/api/v1/ticket/${chatUuid}`, headerRegisterUser);
+}
