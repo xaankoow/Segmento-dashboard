794b84a3b711b3946559e568df1d2baa78b5e834
parsamanesh <stooormix@gmail.com>
2022-11-01 12:46:12 +0330

    remove prope in message component handle sad and happy mode in footer message get part ticket ,status and status color with fun (supportData)

--- src/component/service/ticket.js
+++ src/component/service/ticket.js
@@ -19,3 +19,12 @@ export const getSupportChatData = chatUuid => {
     }
     return http.get(`${config.xaankooApi}/api/v1/ticket/${chatUuid}`, headerRegisterUser);
 }
+
+export const ticketTableData = () => {
+    //7
+    const headerRegisterUser = {
+        'Content-Type': 'multipart/form-data',
+        "workspace":"text/plain"
+    }
+    return http.get(`${config.xaankooApi}/api/v1/ticket?type=&value=`, headerRegisterUser);
+}
