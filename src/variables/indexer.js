c09dc2b31fd641a1e43b1ec5feaada9c271e7205
storm <stooormix@gmail.com>
2022-11-18 13:19:40 +0330

    adding filterIndexerTableItem, indexerLinkStatus and titleOfIndexerTable in the indexer variables

--- src/variables/indexer.js
+++ src/variables/indexer.js
@@ -1,2 +1,40 @@
-export const defaultBoxStyleIndexer="border border-border m-auto py-7 pl-4 rounded-lg mt-10"
+export const defaultBoxStyleIndexer="border border-border m-auto py-7 pl-4 rounded-lg mt-3"
 export const parentHorizontalLineInBeforTextStyleIndexer="w-60 min-w-[240px] inline-block"
+
+//combo search
+export const filterIndexerTabelItem=[
+    {filterName:"url",apiKey:"none"},
+    {filterName:"تاریخ و ساعت آپدیت",apiKey:"none"},
+    {filterName:"تاریخ ثبت",apiKey:"none"},
+    {filterName:"نوع ایندکسر",apiKey:"none"},
+    {filterName:"تناوب ارسال",apiKey:"none"},
+    {filterName:"وضعیت",apiKey:"none"}
+]
+
+export const indexerLinkStatus=[
+    { partName: "ایندکس شده", partCod: 0 },
+    { partName: "در حال انجام", partCod: 0 },
+    { partName: "ایندکس نشده", partCod: 0 }
+    
+]
+
+export const titleOfIndexerTable = [
+    { title: "انتخاب", class: "px-8" },
+    { title: "ردیف", class: "px-8" },
+    { title: "URL", class: "px-8" },
+    { title: "تاریخ و ساعت آپدیت", class: "px-8" },
+    { title: "تاریخ ثبت", class: "px-8" },
+    { title: "نوع ایندکسر", class: "px-8" },
+    { title: "تناوب ارسال", class: "px-8" },
+    { title: "وضعیت", class: "px-8" },
+  ];
+
+  // export const indexerRowKey = [
+  //   "row.id ",
+  //   "row.ticket_id",
+  //   "row.title",
+  //   "row.categories",
+  //   "row.updated_at",
+  //   "row.status",
+  //   "row.operation",
+  // ];
