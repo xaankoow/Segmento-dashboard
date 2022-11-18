bc40cf34ba937d64075b2b9b77ad293a0df179b0
storm <stooormix@gmail.com>
2022-11-18 13:33:06 +0330

    import WorkSpacesInfoPage and ManagementIndexerWorkSpace in GoogleIndexer/index and using

--- src/pages/GoogleIndexer/index.jsx
+++ src/pages/GoogleIndexer/index.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 import PageTitle from '../../component/Dashboard/DashboaedComponents/pageTitle/pageTitle'
 import FirstTimePage from './FirstTimePage'
 import RegisterLinkPage from './RegisterLinkPage'
+import WorkSpaceInfo from './WorkSpacesInfoPage'
+import ManagementIndexerWorkSpace from './ManagementIndexerWorkSpace'
+
 
 export default function GoogleIndexer() {
 
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(4);
 
   const handleShowPage=(step)=>{
     switch (step) {
@@ -15,18 +18,22 @@ export default function GoogleIndexer() {
         //صفحه ثبت لینک ها
         case 2:
         return <RegisterLinkPage/>
+        case 3:
+        return <WorkSpaceInfo/>
+        case 4:
+        return <ManagementIndexerWorkSpace/>
       default:
         break;
     }
   }
   return (
-    <div>
+    <>
         <PageTitle title={"‌ایندکسر گوگل"} badgeApi={"GOOGLE_INDEXER_LIMIT"} hasLimit/>
         <div className='px-10 '>
-
         {handleShowPage(step)}
         </div>
         <index/>
-    </div>
+        
+    </>
   )
 }
