afd3638cd77f731d0a8f9d3c8962066edecf22de
storm <stooormix@gmail.com>
2022-12-02 00:43:11 +0330

    editing setting request for the cancled request

--- src/component/service/httpService.js
+++ src/component/service/httpService.js
@@ -13,7 +13,7 @@ if (token !== "undefined" && token != null && token) {
 
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
-    if (!expectedError) {
+    if (!expectedError&error.code!="ERR_CANCELED") {
         toast.error("مشکلی از سمت سرور رخ داده است.", {
             position: "top-right",
             closeOnClick: true
