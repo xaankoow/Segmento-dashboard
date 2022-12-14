b6dfd9509fa6b791f5d9163fbc9e85b2bd811529
Meysam Jafari <meysam.jafari.bojd@gmail.com>
2022-12-14 06:28:00 +0330

    feat: Add a short path for addressing components

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,21 @@
+const cracoAlias = require('craco-alias')
+
 module.exports = {
   style: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
         require('autoprefixer'),
       ],
     },
   },
+  plugins: [
+    {
+      plugin: cracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: '.',
+        tsConfigPath: './tsconfig.path.json',
+      },
+    },
+  ]
 }
