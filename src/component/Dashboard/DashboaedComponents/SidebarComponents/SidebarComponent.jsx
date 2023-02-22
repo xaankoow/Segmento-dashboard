83df4322ca660ab71f85563539430816fb8e508f
parsamanesh <stooormix@gmail.com>
2023-02-22 11:01:18 +0330

    debugg closing sidebar

--- src/component/Dashboard/DashboaedComponents/SidebarComponents/SidebarComponent.jsx
+++ src/component/Dashboard/DashboaedComponents/SidebarComponents/SidebarComponent.jsx
@@ -53,8 +53,7 @@ useEffect(() => {
   return (
     <>
       <div
-        className="list_hover pt-5 h-full  bg-[#fcfcfb]  shadow-3xl rounded-tl-lg rounded-bl-lg flex flex-col justify-between min-w-[256px] overflow-hidden"
-        style={{ width: closeNav ? "256px" : "0px" }}
+        className={`list_hover pt-5 h-full  bg-[#fcfcfb]  shadow-3xl rounded-tl-lg rounded-bl-lg flex flex-col justify-between overflow-hidden ${closeNav?"min-w-[256px] w-64":"w-0"}`}
       >
         {activeIcon == 0 ? (
           <div>
