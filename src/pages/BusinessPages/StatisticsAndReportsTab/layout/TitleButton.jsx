bd971f28116446710eaf7375d9c32c0e1374000a
parsamanesh <stooormix@gmail.com>
2023-03-11 01:21:04 +0330

    set navigate and show adding key words modal

--- src/pages/BusinessPages/StatisticsAndReportsTab/layout/TitleButton.jsx
+++ src/pages/BusinessPages/StatisticsAndReportsTab/layout/TitleButton.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router';
 import { ImageContainer } from '../../../../assets/img/IMG'
 import AuthButton from '../../../../component/Auth/authButton/AuthButton'
+import AddingBusinessPagesModal from '../../businessPagesTab/layout/AddingBusinessPagesModal'
 
-export default function index() {
+export default function Index() {
+  const [showAddBusindesPageModal, setShowAddBusindesPageModal] = useState(false);
+  const navigate = useNavigate()
+
   return (
     <div className='flex justify-between items-baseline'>
-        {/* <div className=''> */}
-
-        {/* </div> */}
-        <div className=' inline-block'>
+        <div onClick={()=>navigate(-1)} className=' inline-block cursor-pointer'>
             <img src={ImageContainer.blueArrowBtn} alt="arrow" className=' inline-block rotate-180 ml-4'/>
             <span className='text-shortText text-sm'>بازگشت</span>
         </div>
-        <AuthButton textButton={<><img src={ImageContainer.plus} alt="plus" className=' ml-4'/>افزودن صفحه</>} classes="inline-block"/>
+        <AuthButton handlerClick={setShowAddBusindesPageModal} setOnclickValue={true} textButton={<><img src={ImageContainer.plus} alt="plus" className=' ml-4'/>افزودن صفحه</>} classes="inline-block"/>
+        {showAddBusindesPageModal&&<AddingBusinessPagesModal showModal={setShowAddBusindesPageModal}/>}
+   
     </div>
   )
 }
