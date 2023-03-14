6adac2d1b8eb998afdf41251c8ab2d4e208d2763
parsamanesh <stooormix@gmail.com>
2023-03-14 11:54:24 +0330

    adding filter tabel card

--- src/pages/BusinessPages/StatisticsAndReportsTab/StatisticsAndReportsTab.jsx
+++ src/pages/BusinessPages/StatisticsAndReportsTab/StatisticsAndReportsTab.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import TechnicalAnalysis from './layout/TechnicalAnalysis'
 import ChartKeyWordRank from './layout/ChartKeywordRank'
+import FilterTabel from './layout/FIlterTabel'
 
 export default function StatisticsAndReportsTab() {
   return (
-    <div>
+    <div className="px-7">
       <TechnicalAnalysis/>
+      <div className=' mt-7 py-7 px-3 border border-border rounded-lg shadow-[0px_4px_8px_0px_rgb(0,0,0,0.14)]'>
+        <FilterTabel/>
+      </div>
       <div className=' mt-7'>
       <ChartKeyWordRank/>
       </div>
--- src/pages/BusinessPages/StatisticsAndReportsTab/layout/FIlterTabel.jsx
+++ src/pages/BusinessPages/StatisticsAndReportsTab/layout/FIlterTabel.jsx
@@ -1,60 +1,181 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DateObject } from 'react-multi-date-picker';
+import MultiProgress from 'react-multi-progress';
+import { ImageContainer } from '../../../../assets/img/IMG';
 import AuthButton from '../../../../component/Auth/authButton/AuthButton'
 import ComboBox from '../../../../component/shared/comboBox/ComboBox'
+import { BUSINESS_PAGE_FILTER_TABEL_BUSINESS_TAB } from '../../../../variables/businessPages';
+import { sampleChartColors } from '../../../baghShahiRanck/configs/sampleChartData';
+import FilterData from '../../businessPagesTab/layout/FilterSection/FilterData';
 
 export default function Index() {
   const [searchFilterOption, setSearchFilterOption] = useState("شماره فاکتور");
 
-  return (
-    <div>
-                <header className="flex items-center justify-between h-10 w-full mb-7 mt-10">
-          <div className="w-[410px]">
-            <ComboBox
-              placeholder={"فیلد جستجو"}
-              radioTextItems={filterBoxDatas}
-              radioClickedHandler={(e) => setSearchFilterOption(e)}
-            />
-          </div>
-          <div className="flex items-center ">
-            {searchFilterOption !== "بدون فیلتر" && (
-              <span className=" ml-2">مرتب سازی بر اساس</span>
-            )}
+  const [addingKeyWordModal, setAddingKeyWordModal] = useState({key:[],showModal:false});
 
-            <FilterData
-              radioTarget={searchFilterOption}
-              userTypeData={(e) => setUserType(e)}
-              FactorHandler={setFactorHandler}
-              setDatePickerValues={setDatePickerValues}
-              datePickerValues={datePickerValues}
-              priceHandler={setPrice}
-              priceHandler1={setPrice2}
-            />
-          </div>
+  const [deleteItem, setDeleteItem] = useState({data:[],showPopUp:false});
 
-          <div className=" inline-block">
-            <AuthButton
-              textButton={"اعمال"}
-              handlerClick={() =>
-                setFinancialDataTableFiltered(
-                  filterFinancialData(
-                    financialDataTableOrg,
-                    searchFilterOption,
-                    filterItems[searchFilterOption]
-                  )
-                )
-              }
-              setOnclickValue={{
-                textTarget: searchFilterOption,
-                textValue: searchFilterText,
-                sortTarget: targetSortFilter,
-                sortValue:
-                  targetSortFilter == "تاریخ خرید"
-                    ? datePickerValues
-                    : numFilter,
+  const [datePickerValues, setDatePickerValues] = useState([
+    new DateObject().subtract(4, "days"),
+    new DateObject().add(0, "days"),
+  ]);
+
+  const arrayOfTickets = [0, 1, 2, 3, 4, 5, 6, 7].map((item, index) => {
+    return {
+      id: (
+        <p className=" w-11 text-center">
+          <div className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900 ">
+            <input
+              type={"checkbox"}
+              className="checkbox rounded border border-[#D9D9D9] bg-[#0A65CD] w-[18px] h-[18px] cursor-pointer hover:border-[#0A65CD] hover:border"
+              // className="checkbox rounded border border-[#D9D9D9] bg-[#FCFCFB] w-[18px] h-[18px] cursor-pointer hover:border-[#0A65CD] hover:border"
+              onClick={(e) => {
+                if (e.target.checked) {
+                  setDeleteItem({data:[...deleteItem.data, item],showPopUp:false});
+                } else {
+                  setDeleteItem(
+                    {data:deleteItem.data.filter((copyItems) => copyItems != item),
+                    showPopUp:false}
+                  );
+                }
+                
+                // handleCheckingInput(e.target.checked, item);
               }}
             />
           </div>
-        </header>
+        </p>
+      ),
+      ticket_id: index + 1,
+      title: "https://segmento.ir/google-indexer",
+      categories: (
+        <AuthButton
+          textButton={
+            <img
+              src={ImageContainer.bluePlus}
+              alt="blue plus"
+              className="p-1"
+            />
+          }
+          handlerClick={setAddingKeyWordModal}
+          setOnclickValue={{key:[item],showModal:true}}
+          classes="btn-secondary m-auto"
+        />
+      ),
+      updated_at: "2",
+      status: "1401/02/20",
+      operation: (
+        <MultiProgress
+          transitionTime={1.2}
+          height="10px"
+          // className=" overflow-visible-force"
+          elements={[
+            {
+              // actual:65,
+              value: 35,
+              color: "#D9D9D9",
+              showPercentage: false,
+              fontSize: 9,
+              textColor: "black",
+              isBold: false,
+            },
+            {
+              actual: 65,
+
+              value: 65,
+              color: "#10CCAE",
+              showPercentage: false,
+              // textColor: "white",
+              fontSize: 9,
+              isBold: false,
+              // className: "my-custom-css-class",
+            },
+          ]}
+          component={({ children, element, ...rest }) => {
+            // console.log(children, element, "iiiiiii");
+
+            // console.log({ xx }); progressBar__single
+            return (
+              <>
+                <div
+                  {...rest} // adds all styles for rendering the progress bar
+                  style={{
+                    fontWeight: element.isBold ? 900 : 300,
+                  }}>
+                  {children}
+                  <div className="text-xs">
+                    <span className="ml-3 text-s absolute left-0 -top-6">
+                      {element?.actual}
+                    </span>
+                    <i
+                      className={` inline-block ${
+                        element.color === sampleChartColors.failure
+                          ? "borderBT--red triangle-down"
+                          : element.color === sampleChartColors.success
+                          ? "triangle-up"
+                          : "dot"
+                      }`}></i>
+                  </div>
+                </div>
+              </>
+            );
+          }}
+        />
+      ),
+      moreInfo: (
+        <AuthButton
+          textButton={<img src={ImageContainer.blueArrowBtn} />}
+          classes="btn-secondary"
+        />
+      ),
+    };
+  });
+
+  const rowKey = [
+    "row.id ",
+    "row.ticket_id",
+    "row.title",
+    "row.categories",
+    "row.updated_at",
+    "row.status",
+    "row.operation",
+    "row.moreInfo",
+  ];
+
+  return (
+    <header className="flex items-center justify-between h-10 w-full ">
+    <div className=" w-96">
+      <ComboBox
+        placeholder={"فیلد جستجو"}
+        radioTextItems={BUSINESS_PAGE_FILTER_TABEL_BUSINESS_TAB}
+        radioClickedHandler={(e) => setSearchFilterOption(e)}
+      />
     </div>
+    <div className="flex items-center ">
+      {searchFilterOption !== "بدون فیلتر" && (
+        <span className=" ml-2">مرتب سازی بر اساس</span>
+      )}
+
+      <div className=" inline">
+        <FilterData
+          radioTarget={searchFilterOption}
+          datePickerValues={datePickerValues}
+          inputWidth="300px"
+        />
+      </div>
+
+      {/* <FilterData
+          userTypeData={(e) => setUserType(e)}
+          FactorHandler={setFactorHandler}
+          setDatePickerValues={setDatePickerValues}
+          datePickerValues={datePickerValues}
+          priceHandler={setPrice}
+          priceHandler1={setPrice2}
+        /> */}
+    </div>
+
+    <div className=" inline-block">
+      <AuthButton textButton={"اعمال"} />
+    </div>
+  </header>
   )
 }
