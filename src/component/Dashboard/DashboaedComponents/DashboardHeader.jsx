4770d71901681e7d559a8ab279de550350becde1
babak-x <babak.xaankoo@gmail.com>
2023-03-16 11:33:28 +0330

    'segmento' removed from header.

--- src/component/Dashboard/DashboaedComponents/DashboardHeader.jsx
+++ src/component/Dashboard/DashboaedComponents/DashboardHeader.jsx
@@ -138,7 +138,7 @@ const DashboardHeader = ({ setActiveIconHandlerClicked, setClicked1 }) => {
             {/* <div className='Iconimage w-7 h-8'></div> */}
             <AnimationSegmentoLogo parentClass={"w-7 h-7 "} animation={ProcessingDelay?.length>0|ImportantProcessingDelay.length>0?true:false} animationWithHover/>
             {/* <img src={segmento_logofa_svg} className="w-7 h-8" alt="" /> */}
-            <span className="">سگمنتو segmento</span>
+            <span className="">سگمنتو</span>
           </div>
         </a>
       </div>
