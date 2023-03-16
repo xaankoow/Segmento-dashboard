1f508ce0acf2bc9356697d7ca97a0769a1ce83eb
parsamanesh <stooormix@gmail.com>
2023-03-16 15:03:07 +0330

    debugg changed plan details (bugg in discountTagValue)

--- src/component/Utils/buyPlanSection_UTILS/DiscountTagValue.jsx
+++ src/component/Utils/buyPlanSection_UTILS/DiscountTagValue.jsx
@@ -1,26 +1,45 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { setDiscountPrice, setFormatPrice } from "../FORMAT/price";
 
-export default function DiscountTagValue({ tagStatusName, price, planSelected ,defaultDiscount}) {
-    // default_discount_price
-    const { discountStatus ,allPackageData } = useSelector(state => state.planState);
-    var originalPrice = "";
-    var priceAfterTheDiscount = setFormatPrice(price.default_discount_price) + "  تومان";
+export default function DiscountTagValue({ tagStatusName, price, planSelected }) {
+    const { discountStatus } = useSelector(state => state.planState);
 
-    if (discountStatus.value != 0 && discountStatus.value>price.default_discount) {
-        if (discountStatus.planType == tagStatusName) {
-            originalPrice = setFormatPrice(price.default_discount_price) + "  تومان"
-            priceAfterTheDiscount = setFormatPrice(setDiscountPrice(price.default_discount_price, discountStatus.value, discountStatus.discountType == "cash" ? true : false).value) + "  تومان";
+    const [preview,setPreview]=useState({
+        originalPrice:"",
+        priceAfterTheDiscount:setFormatPrice(price.default_discount_price) + "  تومان"
+    })
+    // var preview.originalPrice = "";
+    // var preview.priceAfterTheDiscount = setFormatPrice(price.default_discount_price) + "  تومان";
+
+    
+    useEffect(() => {
+        let funDisValue = setDiscountPrice(price.price, discountStatus.value, discountStatus.discountType == "cash" ? true : false);
+    
+        if (discountStatus.value != 0 && funDisValue.value<price.default_discount) {
+            if (discountStatus.planType == tagStatusName) {
+                setPreview({
+                    originalPrice:setFormatPrice(price.price) + "  تومان",
+                    priceAfterTheDiscount:setFormatPrice(setDiscountPrice(price.price, discountStatus.value, discountStatus.discountType == "cash" ? true : false).value) + "  تومان"
+                })
+                // preview.originalPrice = setFormatPrice(price.price) + "  تومان"
+                // preview.priceAfterTheDiscount = setFormatPrice(setDiscountPrice(price.price, discountStatus.value, discountStatus.discountType == "cash" ? true : false).value) + "  تومان";
+            }
+        }else if (price.default_discount!=undefined&&price.default_discount!=0) {
+            setPreview({
+                originalPrice:setFormatPrice(price.price) + "  تومان",
+                priceAfterTheDiscount:setFormatPrice(price.default_discount_price) + "  تومان"
+            })
+            // preview.originalPrice = setFormatPrice(price.price) + "  تومان"
+            // preview.priceAfterTheDiscount = setFormatPrice(price.default_discount_price) + "  تومان";
         }
-    }else if (price.default_discount!=undefined&&price.default_discount!=0) {
-        originalPrice = setFormatPrice(price.price) + "  تومان"
-        priceAfterTheDiscount = setFormatPrice(price.default_discount_price) + "  تومان";
-    }
+    }, [discountStatus.value,planSelected,])
+    
 
     return (
         <div className='price relative'>
-            <span className=' absolute bottom-12 line-through w-full text-xs right-0'>{originalPrice}</span>
-            <p style={planSelected.type.substring(0, 1) == tagStatusName.substring(0, 1) ? { color: "rgba(10, 101, 205, 1)" } : null}>{priceAfterTheDiscount}</p>
+            <span className=' absolute bottom-12 line-through w-full text-xs right-0'>{preview.originalPrice}</span>
+            <p style={planSelected.type.substring(0, 1) == tagStatusName.substring(0, 1) ? { color: "rgba(10, 101, 205, 1)" } : null}>{preview.priceAfterTheDiscount}</p>
         </div>
     )
 }
