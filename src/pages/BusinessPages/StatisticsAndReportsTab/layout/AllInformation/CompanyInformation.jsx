cdf7e781225685dc0e4a24ccb20f0eeea8533ad5
parsamanesh <stooormix@gmail.com>
2023-05-09 00:32:18 +0330

    set data for company chart in all information business pages adding picture for all information in the business pages section

--- src/pages/BusinessPages/StatisticsAndReportsTab/layout/AllInformation/CompanyInformation.jsx
+++ src/pages/BusinessPages/StatisticsAndReportsTab/layout/AllInformation/CompanyInformation.jsx
@@ -1,30 +1,48 @@
 import React from "react";
-// import { Doughnut } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 // import RotateLine from "../../../../../component/shared/rotateLine";
 
-export default function index({ companyName, link, description }) {
-  // const miniChartSetting2 = {
-  //   datasets: [
-  //     {
-  //       label: "# of Votes",
-  //       data: [0, 1],
-  //       cutout: 55,
-  //       backgroundColor: ["#D9D9D9", "#10CCAE"],
-  //       borderWidth: 0,
-  //       borderRadius: 5,
-  //     },
-  //   ],
-  // };
+export default function index({
+  companyName,
+  link,
+  description,
+  picture,
+  chartData,
+}) {
+  const miniChartSetting2 = {
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [100, chartData],
+        cutout: 55,
+        backgroundColor: ["#D9D9D9", "#10CCAE"],
+        borderWidth: 0,
+        borderRadius: 5,
+      },
+    ],
+  };
+  console.log('chartData', chartData)
 
   return (
     <div className="flex justify-between relative h-40 px-7">
       {/* //w-40 */}
-      {/* <div className=" w-1/6">
+      {/* <div className="w-40 relative">
+          <div className="flex justify-center items-center h-full w-full absolute text-4xl left-0 leading-5 text-center">
+            {chartData.datasets[0].data[0]}
+          </div>
+          <Doughnut data={chartData} options={{ maintainAspectRatio: false }} />
+        </div>
+        <p className="m-auto mt-7 text-title text-lg">{title}</p> */}
+      <div className="relative w-1/6">
+        <div className="flex justify-center items-center h-full w-full absolute text-4xl left-0 leading-5 text-center">
+          {chartData}
+        </div>
         <Doughnut
           data={miniChartSetting2}
           options={{ maintainAspectRatio: false }}
         />
-      </div> */}
+        {/* <p className="m-auto mt-7 text-title text-lg">{chartData}</p> */}
+      </div>
       <div className="h-full flex flex-col justify-between items-start w-4/6 mr-9">
         <p className=" text-lg text-title">{companyName}</p>
         <p className="text-gray text-sm text-right">{description}</p>
@@ -34,7 +52,9 @@ export default function index({ companyName, link, description }) {
       </div>
       {/* <RotateLine/> */}
       <div className=" border border-border h-full mx-7"></div>
-      <div className=" flex-grow h-full border border-border w-2/6 min-w-[265px]"></div>
+      <div className=" relative flex-grow h-full border border-border w-2/6 min-w-[265px]">
+        <img src={picture} alt="company picture" className="w-full h-full" />
+      </div>
     </div>
   );
 }
--- src/pages/BusinessPages/StatisticsAndReportsTab/layout/AllInformation/index.jsx
+++ src/pages/BusinessPages/StatisticsAndReportsTab/layout/AllInformation/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CompanyInformation from "./CompanyInformation";
-import CompanyDetails from "./CompanyDetails";
 import SpeedInformation from "./SpeedInformation";
 import ChartsData from "./ChartsData";
-import PagesLoading from "./PagesLoading";
 export default function index({ pageData, insight }) {
+
+  const getAvgRankForCompanyInfoChart=()=>{
+    let avg;
+    avg=[insight[0].performance,insight[0].accessibility,insight[0].best_practices,insight[0].seo].reduce((prev,value)=>parseInt(prev)+parseInt(value));
+    return avg
+  }
   return (
     <div className="rounded-lg w-full border border-border overflow-hidden">
       <div className="text-title text-sm px-4 py-2  bg-white w-full text-right">
@@ -15,6 +19,8 @@ export default function index({ pageData, insight }) {
           companyName={pageData?.title}
           link={pageData?.link}
           description={pageData?.description}
+          picture={pageData?.shot}
+          chartData={insight && getAvgRankForCompanyInfoChart()}
         />
       </div>
       {/* <div className=' mt-10'>
