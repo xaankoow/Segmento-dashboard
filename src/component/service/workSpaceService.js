a6c698a0c76b4f8419c0cada49a41dcc9385d62e
Ali Astan <ali.astanboos@gmail.com>
2023-05-23 16:13:01 +0330

    Change new fake data with old data in workspace service

--- src/component/service/workSpaceService.js
+++ src/component/service/workSpaceService.js
@@ -1,30 +1,43 @@
-import http from './httpService'
-import config from './config.json'
+import http from "./httpService";
+import config from "./config.json";
 
-export const website = website => {
-    const headerRegisterUser = {
-        "Accept": "application/json"
-    }
-    return http.post(`${config.xaankooApi}/api/v1/workspace`,website,headerRegisterUser);
-}
+export const website = (website) => {
+  const headerRegisterUser = {
+    Accept: "application/json",
+  };
+  return http.post(
+    `${config.xaankooApi}/api/v1/workspace`,
+    website,
+    headerRegisterUser
+  );
+};
 
 export const getAllWorkspace = () => {
-    const headerRegisterUser = {
-        "Accept": "application/json"
-    }
-    return http.get(`${config.xaankooApi}/api/v1/workspace`,headerRegisterUser);
-}
+  const headerRegisterUser = {
+    Accept: "application/json",
+  };
+  let response = http.get(`${config.xaankooApi}/api/v1/workspace`, headerRegisterUser);
+  return response;
+};
 
-export const keywords = website => {
-    const headerRegisterUser = {
-        "Accept": "application/json"
-    }
-    return http.post(`${config.xaankooApi}/api/v1/workspace`,website,headerRegisterUser);
-}
+export const keywords = (website) => {
+  const headerRegisterUser = {
+    Accept: "application/json",
+  };
+  return http.post(
+    `${config.xaankooApi}/api/v1/workspace`,
+    website,
+    headerRegisterUser
+  );
+};
 
-export const creatWorkSpace = workSpace => {
-    const headerRegisterUser = {
-        "Accept": "application/json"
-    }
-    return http.post(`${config.xaankooApi}/api/v1/workspace/workspace-wizard`,workSpace,headerRegisterUser);
-}
+export const creatWorkSpace = (workSpace) => {
+  const headerRegisterUser = {
+    Accept: "application/json",
+  };
+  return http.post(
+    `${config.xaankooApi}/api/v1/workspace/workspace-wizard`,
+    workSpace,
+    headerRegisterUser
+  );
+};
